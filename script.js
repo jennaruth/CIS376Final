c1cd80f54a1d40ab45f720182de2480bb10163a3
jennaruth <47092497+jennaruth@users.noreply.github.com>
2020-05-03 20:15:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload=function(){
+  document.getElementById("linkid").click();
+};
+
+ function doSomething(){
+  alert("HTML5, Markdown, CSS3, JavaScript/ECMAScript, jQuery, Bootstrap 4)")
+}
+
+
+window.onload=function(){
+  document.getElementById("des2").click();
+};
+
+function link(){
+  alert("DevDocs, W3CSchools, CodeAcademy, Content Delivery Networks, Documentation, Templates;  GitHub, Command Line Interface, Wave, Browser Developer Tools, Keyboard Shortcuts, Code Editors")
+}
+
+window.onload=function(){
+  document.getElementById("des3").click();
+};
+
+function link2(){
+  alert("Web Developer lexicon; markup, page layouts, behavior, and interaction; Document Object Model manipulation, JavaScript Object Notation; Web Dev Ecosystem; File naming, directory/repository configuration, compression, exporting/cloning, hosting configuration/architecture; testing & debugging; Standards: Google Style Guide, W3C Accessibility Standards")
+}
